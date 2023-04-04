e1870d24ab3ca8bfcd9a1a2bf019ff28486ade79
pc <getiebalew2020@gmail.com>
2023-04-04 16:38:34 +0300

    Task 0 Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,11 +4,10 @@
 import asyncio
 import random
 from typing import Generator
-# Generator[yield_type, send_type, return_type]
 
 
 async def async_generator() -> Generator[float, None, None]:
-    '''Coroutine that prints a sequence of 10 random numbers.
+    '''Coroutine that prints a sequence of 10 random numbers using random module.
     '''
     for _ in range(10):
         await asyncio.sleep(1)
