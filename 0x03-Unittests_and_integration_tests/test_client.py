eadd261ea9e4088722f5d7d69f87e1788188ee84
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-24 15:29:11 -0400

    testing github org client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,8 +11,9 @@ from parameterized import parameterized, parameterized_class
 import json
 import unittest
 from unittest.mock import patch, PropertyMock, Mock
-
-
+from client import (
+    GithubOrgClient
+)
 class TestGithubOrgClient(unittest.TestCase):
     """Testing Github org Client """
     #
@@ -79,21 +80,16 @@ class TestGithubOrgClient(unittest.TestCase):
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Performs integration tests for the `GithubOrgClient` class."""
     @classmethod
-    def setUpClass(cls) -> None:
-        """Sets up class fixtures before running tests."""
-        route_payload = {
-            'https://api.github.com/orgs/google': cls.org_payload,
-            'https://api.github.com/orgs/google/repos': cls.repos_payload,
-        }
-
-        def get_payload(url):
-            if url in route_payload:
-                return Mock(**{'json.return_value': route_payload[url]})
-            return HTTPError
-
-        cls.get_patcher = patch("requests.get", side_effect=get_payload)
-        cls.get_patcher.start()
-
+    def setUpClass(cls):
+        """A class method called before tests in an individual class are run"""
+        config = {'return_value.json.side_effect':
+                  [
+                      cls.org_payload, cls.repos_payload,
+                      cls.org_payload, cls.repos_payload
+                  ]
+                  }
+        cls.get_patcher = patch('requests.get', **config)
+        cls.mock = cls.get_patcher.start()
     def test_public_repos(self) -> None:
         """Tests the `public_repos` method."""
         self.assertEqual(
