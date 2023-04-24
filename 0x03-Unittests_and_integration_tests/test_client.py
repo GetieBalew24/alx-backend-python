e43e837934cb123a92bebf14d51a8af1c571ac23
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-24 14:31:19 -0400

    testing github org client

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Module for testing client
+Use @patch as a decorator to make sure get_json is called once with
+the expected argument but make sure it is not executed.
+Use @parameterized.expand as a decorator to parametrize 
+the test with a couple of org examples to pass to GithubOrgClient"""
+
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
+import json
+import unittest
+from unittest.mock import patch, PropertyMock, Mock
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Testing Github org Client """
+    #
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """implement the test_org method
+        that tests GithubOrgClient.org returns the correct value"""
+        GithubOrgClient(input).org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
